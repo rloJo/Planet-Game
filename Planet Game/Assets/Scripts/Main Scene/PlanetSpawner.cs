1b5883f84903462e52676863ee4bee4d704f226b
LeeEunWoo <lew991216@gmail.com>
2024-03-25 20:46:21 +0900

    Feat : 행성 생성 및 스폰위치 설정
    행성 생성 및 스폰 위치 이동 기능 추가

---
+++ Planet Game/Assets/Scripts/Main Scene/PlanetSpawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour
+{
+    public float horizontalInput;
+    private float speed = 7.0f;
+    private Vector3 pos;
+    private float xRange = 3.3f;
+    private bool isDead = false;
+    public GameObject[] planets;
+    // Start is called before the first frame update
+
+    private void Spawn() 
+    { 
+        Vector3 spawnPosition = transform.position;
+        int rand = Random.Range(1, 101);
+        int idx = 0;
+
+        if (rand <= 28) idx = 0;
+        else if (rand <= 58) idx = 1;
+        else if (rand <= 85) idx = 2;
+        else idx = 3;
+
+        GameObject SelectedPlanet = planets[idx];
+        GameObject planet = Instantiate(SelectedPlanet, spawnPosition, Quaternion.identity);
+    
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead) {
+            if(transform.position.x < -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, 0);
+            }
+            else if (transform.position.x > xRange)
+            {
+                transform.position = new Vector3(xRange, transform.position.y, 0);
+            }
+            else
+            {
+                horizontalInput = Input.GetAxis("Horizontal");
+                transform.Translate(Vector3.right *Time.deltaTime * speed * horizontalInput);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Spawn();
+            }
+        }
+    }
+}
